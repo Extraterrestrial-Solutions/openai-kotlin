a2734a431a3b18f555f0638eefba484539dc0273
Artem Pavlichenko <pavlichenko@siberian.pro>
2025-06-19 18:37:50 +0300

    4.0.1-ET2: Исправлена json_schema для запроса
    https://github.com/AdrianMiska/openai-kotlin/tree/feature/Responses-API

--- openai-core/src/commonMain/kotlin/com.aallam.openai.api/responses/ResponseTextConfig.kt
+++ openai-core/src/commonMain/kotlin/com.aallam.openai.api/responses/ResponseTextConfig.kt
@@ -41,8 +41,29 @@ public data object JsonObjectFormat : TextResponseFormatConfiguration
 @Serializable
 @SerialName("json_schema")
 public data class JsonSchemaFormat(
-    /** Structured Outputs configuration options, including a JSON Schema */
-    @SerialName("json_schema") val jsonSchema: ResponseJsonSchema
+    /**
+     * A description of what the response format is for, used by the model to
+     * determine how to respond in the format.
+     */
+    @SerialName("description") val description: String? = null,
+
+    /**
+     * The name of the response format. Must be a-z, A-Z, 0-9, or contain
+     * underscores and dashes, with a maximum length of 64.
+     */
+    @SerialName("name") val name: String? = null,
+
+    /**
+     * The schema for the response format, described as a JSON Schema object.
+     */
+    @SerialName("schema") val schema: JsonObject,
+
+    /**
+     * Whether to enable strict schema adherence when generating the output.
+     * If set to true, the model will always follow the exact schema defined
+     * in the `schema` field.
+     */
+    @SerialName("strict") val strict: Boolean? = null
 ) : TextResponseFormatConfiguration
 
 /**
